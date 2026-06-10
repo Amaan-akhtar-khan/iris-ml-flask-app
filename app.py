51b3088b5b1fa7b164fcf924c3b301f0847ca789
Amaan Akhtar Khan <k.amaanakhtar@gmail.com>
2026-06-10 11:31:33 +0530

    Made mobile version and about page

--- app.py
+++ app.py
@@ -16,6 +16,10 @@ model = joblib.load("iris_model.pkl")
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
